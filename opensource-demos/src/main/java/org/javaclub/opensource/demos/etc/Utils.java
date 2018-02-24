85d83a93c05372dabe11f8dc84a2f0ea55387ecf
jadmin <gerald.chen.hz@gmail.com>
2018-02-24 11:21:40 +0800

    cccccc

--- opensource-demos/src/main/java/org/javaclub/opensource/demos/etc/Utils.java
+++ opensource-demos/src/main/java/org/javaclub/opensource/demos/etc/Utils.java
@@ -17,6 +17,8 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.StringReader;
 import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
@@ -343,6 +345,47 @@ public class Utils {
         }
         return (String[]) list.toArray(new String[list.size()]);
     }
+	
+	/**
+	 * 计算md5
+	 */
+	public static String getMd5(final String text) {
+        return md5(text.getBytes());
+    }
+	
+	/**
+     * Make MD5 diaguest.
+     */
+    public static String md5(byte[] data) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] buf = md.digest(data);
+            return toHexString(buf);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("MD5 Algorithm not supported", e);
+        }
+    }
+    
+    public static String toHexString(byte[] bytes) {
+        int length = bytes.length;
+        StringBuffer sb = new StringBuffer(length * 2);
+        int x = 0;
+        int n1 = 0, n2 = 0;
+        for(int i=0; i<length; i++) {
+            if(bytes[i]>=0)
+                x = bytes[i];
+            else
+                x= 256 + bytes[i];
+            n1 = x >> 4;
+            n2 = x & 0x0f;
+            sb = sb.append(HEX[n1]);
+            sb = sb.append(HEX[n2]);
+        }
+        return sb.toString();
+    }
+
+    private static final char[] HEX = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+    
 	
 	// ===== Strings end =======
 	
